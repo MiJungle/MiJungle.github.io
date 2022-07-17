98b94f488d1ac9f7bb042698613d816f2982cf31
Mijungle <77824583+MiJungle@users.noreply.github.com>
2022-07-17 16:59:11 +0900

    Add files via upload

---
+++ precache-manifest.0707e5ce1f05a8d7cff7dd464557732b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d58e4cd5299474f6f638",
+    "url": "/static/css/main.7a026f88.chunk.css"
+  },
+  {
+    "revision": "d58e4cd5299474f6f638",
+    "url": "/static/js/main.d58e4cd5.chunk.js"
+  },
+  {
+    "revision": "caa6e45e9aaf55938d2f",
+    "url": "/static/js/1.caa6e45e.chunk.js"
+  },
+  {
+    "revision": "d664b49026e13528a3e0",
+    "url": "/static/css/2.734b449b.chunk.css"
+  },
+  {
+    "revision": "d664b49026e13528a3e0",
+    "url": "/static/js/2.d664b490.chunk.js"
+  },
+  {
+    "revision": "c430968dc82bf8ddb573",
+    "url": "/static/js/runtime~main.c430968d.js"
+  },
+  {
+    "revision": "b1d59878bb3dcadd93f782dcb7a90897",
+    "url": "/static/media/video_thumbnail.b1d59878.png"
+  },
+  {
+    "revision": "34e0815a79fc8b7061c1ecb50e9d34fd",
+    "url": "/static/media/main-image.34e0815a.png"
+  },
+  {
+    "revision": "533a198eb5ca9b2b5a9e40fc97d95f5d",
+    "url": "/static/media/desk2.533a198e.png"
+  },
+  {
+    "revision": "256c02d9862695cf5b1a02443919efab",
+    "url": "/index.html"
+  }
+];
